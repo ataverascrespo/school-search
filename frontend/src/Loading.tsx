afd56580a42d2d47c7a82976673588dc675f156d
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2024-10-17 15:09:55 -0400

    Update loading page margins

--- frontend/src/Loading.tsx
+++ frontend/src/Loading.tsx
@@ -8,7 +8,7 @@ export const Icons = {
 export function Loading() {
     return (
         <div className="container">
-            <div className={`w-full h-screen flex flex-col justify-center items-center gap-4 text-neutral-800`}>
+            <div className={`w-full h-[75vh] flex flex-col justify-center items-center gap-4 text-neutral-800`}>
                 <Icons.spinner className="w-[10vw] h-[10vh] animate-spin" />
                 <h2 className="text-xl text-center">Loading</h2>
             </div>
